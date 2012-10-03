0efbe8ea3e6b99dcf70716b9d746fdfacaf0a38d
Imran Rashid <imran@quantifind.com>
2012-10-03 07:43:49 -0700

    get rid of old optional code

--- src/main/scala/examples/erroneous.scala
+++
@@ -1,7 +0,0 @@
-package optional.examples
-
-object Erroneous extends optional.Application
-{
-  def main(times: Option[Int], greeting: Option[String], file: java.io.File) { }
-  def main(dingle: Option[Int]) {}
-}
