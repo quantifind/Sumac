08aa960b4287ad2440035a006b75abdc592ca92f
Imran Rashid <imran@quantifind.com>
2012-12-04 07:37:04 -0800

    working version of ArgApp

--- src/main/scala/optional/ArgApp.scala
+++ src/main/scala/optional/ArgApp.scala
@@ -1,21 +1,40 @@
 package optional
 
+import java.lang.reflect.{ParameterizedType, Type}
+
 trait ArgApp[T <: FieldParsing] {
 
-//  unfortunately, can't get this to work. can't make manifest available :(
-//  def main(rawArgs: Array[String]) {
-//    mainHelper(rawArgs)
-//  }
+  def main(rawArgs: Array[String]) {
+    mainHelper(rawArgs)
+  }
 
-  def mainHelper(rawArgs: Array[String])(implicit m: Manifest[T]) {
-    val args = m.erasure.newInstance().asInstanceOf[T]
+  private def getArgumentClass() = {
+    val argApp = this.getClass.getGenericInterfaces.find{tpe =>
+      val ptpe = tpe.asInstanceOf[ParameterizedType]
+      ParseHelper.checkType(ptpe, classOf[ArgApp[_]])
+    }
+    getRawClass(argApp.get.asInstanceOf[ParameterizedType].getActualTypeArguments.apply(0))
+  }
+
+  private def getRawClass(tpe: Type) = {
+    tpe match {
+      case x:Class[_] => x
+      case p:ParameterizedType => p.getRawType.asInstanceOf[Class[_]]
+    }
+  }
+
+  private def mainHelper(rawArgs: Array[String]) {
+    val argClass = getArgumentClass()
+    val ctors = argClass.getDeclaredConstructors()
+    val ctor = ctors.find(ctor => ctor.getGenericParameterTypes.length == 0).get
+    ctor.setAccessible(true)
+    val args = ctor.newInstance().asInstanceOf[T]
     args.parse(rawArgs)
     main(args)
   }
 
   def main(args: T) : Unit
 }
-
 
 //below is just for testing, but want it in compiled classes ...
 
@@ -25,9 +44,6 @@ class MyArgs extends FieldParsing {
 }
 
 object MyApp extends ArgApp[MyArgs] {
-  def main(args: Array[String]) {
-    mainHelper(args)
-  }
   def main(args: MyArgs) {
     println(args.a)
     println(args.b)
