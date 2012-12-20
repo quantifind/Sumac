0911ec4aa85a8c2ee4d1bbd62d5c2ab080325da6
Imran Rashid <imran@quantifind.com>
2012-12-19 19:11:49 -0800

    some renaming and adding to "ArgApp"

--- src/main/scala/optional/ArgApp.scala
+++ src/main/scala/optional/ArgApp.scala
@@ -2,13 +2,9 @@ package optional
 
 import java.lang.reflect.{ParameterizedType, Type}
 
-trait ArgApp[T <: FieldParsing] {
+trait Argable[T <: FieldParsing] {
 
-  def main(rawArgs: Array[String]) {
-    mainHelper(rawArgs)
-  }
-
-  private lazy val argHolder = {
+  protected lazy val argHolder = {
     val argClass = getArgumentClass()
     val ctors = argClass.getDeclaredConstructors()
     val ctor = ctors.find(ctor => ctor.getGenericParameterTypes.length == 0).get
@@ -28,7 +24,7 @@ trait ArgApp[T <: FieldParsing] {
     val argApp = this.getClass.getGenericInterfaces.find{tpe =>
       tpe match {
         case ptpe: ParameterizedType =>
-          ParseHelper.checkType(ptpe, classOf[ArgApp[_]])
+          ParseHelper.checkType(ptpe, classOf[Argable[_]])
         case _ => false
       }
     }
@@ -42,12 +38,29 @@ trait ArgApp[T <: FieldParsing] {
     }
   }
 
+}
+
+trait ArgMain[T <: FieldParsing] extends Argable[T] {
+  def main(rawArgs: Array[String]) {
+    mainHelper(rawArgs)
+  }
+
   private def mainHelper(rawArgs: Array[String]) {
     argHolder.parse(rawArgs)
     main(argHolder)
   }
 
   def main(args: T) : Unit
+}
+
+trait ArgFunction[T <: FieldParsing, U] extends Function[T,U]
+
+trait ArgApp[T <: FieldParsing] extends Argable[T] with App {
+  override
+  def main(args: Array[String]) {
+    argHolder.parse(args)
+    super.main(args)
+  }
 }
 
 //below is just for testing, but want it in compiled classes ...
@@ -57,9 +70,14 @@ class MyArgs extends FieldParsing {
   val b: Int = 0
 }
 
-object MyApp extends ArgApp[MyArgs] {
+object MyMain extends ArgMain[MyArgs] {
   def main(args: MyArgs) {
     println(args.a)
     println(args.b)
   }
+}
+
+object MyApp extends ArgApp[MyArgs] {
+  println(argHolder.a)
+  println(argHolder.b)
 }
