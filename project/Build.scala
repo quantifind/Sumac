9cb8f84eed3c86e82c001ecf8fc61835fd37caf1
Imran Rashid <imran@quantifind.com>
2013-04-06 10:42:15 -0700

    setup zk project

--- project/Build.scala
+++ project/Build.scala
@@ -1,8 +1,9 @@
 import sbt._
 import Keys._
 
-object SparkBuild extends Build {
-  lazy val core = Project("core", file("."), settings = coreSettings)
+object SumacBuild extends Build {
+  lazy val core = Project("core", file("core"), settings = coreSettings)
+  lazy val zk = Project("zk", file("zk"), settings = zkSettings)
 
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     version := "0.1-SNAPSHOT",
@@ -14,22 +15,30 @@ object SparkBuild extends Build {
     transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
     publishTo <<= baseDirectory { base => Some(Resolver.file("Local", base / "target" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern))) },
     libraryDependencies ++= Seq(
-      "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+      "org.scalatest" %% "scalatest" % "1.6.1" % "test",
+      "log4j" % "log4j" % "1.2.16",
+      "org.slf4j" % "slf4j-api" % slf4jVersion,
+      "org.slf4j" % "slf4j-log4j12" % slf4jVersion
+    ),
+    resolvers ++= Seq(
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/"
     )
   )
 
   val slf4jVersion = "1.6.1"
 
   def coreSettings = sharedSettings ++ Seq(
-    name := "Sumac",
+    name := "Sumac"
+  )
+  
+  def zkSettings = sharedSettings ++ Seq(
+    name := "Sumac-zk",
     resolvers ++= Seq(
-      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-      "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/"
+      "Twitter Repo" at "http://maven.twttr.com/"
     ),
     libraryDependencies ++= Seq(
-      "log4j" % "log4j" % "1.2.16",
-      "org.slf4j" % "slf4j-api" % slf4jVersion,
-      "org.slf4j" % "slf4j-log4j12" % slf4jVersion
+      "com.twitter"   % "util-zk"   % "5.3.10"
     )
   )
 
