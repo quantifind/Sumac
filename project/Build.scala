2f100d6e611b6166338cfe5c22657c1613b96bc9
Imran Rashid <imran@quantifind.com>
2013-05-05 12:00:13 -0700

    cleanup build file a bit

--- project/Build.scala
+++ project/Build.scala
@@ -9,16 +9,13 @@ object SumacBuild extends Build {
     version := "0.2-SNAPSHOT",
     scalaVersion := "2.9.3",
     organization := "com.quantifind",
-    scalacOptions := Seq(/*"-deprecation",*/ "-unchecked", "-optimize"), // -deprecation is too noisy due to usage of old Hadoop API, enable it once that's no longer an issue
+    scalacOptions := Seq("-deprecation", "-unchecked", "-optimize"), 
     unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
     retrieveManaged := true,
     transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
     publishTo <<= baseDirectory { base => Some(Resolver.file("Local", base / "target" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern))) },
     libraryDependencies ++= Seq(
-      "org.scalatest" %% "scalatest" % "1.9.1" % "test",
-      "log4j" % "log4j" % "1.2.16",
-      "org.slf4j" % "slf4j-api" % slf4jVersion,
-      "org.slf4j" % "slf4j-log4j12" % slf4jVersion
+      "org.scalatest" %% "scalatest" % "1.9.1" % "test"
     ),
     resolvers ++= Seq(
       "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
