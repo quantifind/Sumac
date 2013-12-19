3bc1ce1e36629285c3823644a1ba3c16dafefac6
Pierre Andrews <pierre.andrews@gmail.com>
2013-12-19 13:33:34 -0200

    test with more complex parsing types

--- ext/src/test/scala/com/quantifind/sumac/ConfigTest.scala
+++ ext/src/test/scala/com/quantifind/sumac/ConfigTest.scala
@@ -3,6 +3,7 @@ package com.quantifind.sumac
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 import com.typesafe.config.ConfigFactory
+import scala.concurrent.duration._
 
 /**
  * Test ConfigArgs
@@ -21,32 +22,32 @@ class ConfigTest extends FunSuite with ShouldMatchers {
     val test = new Test
     test.parse(args)
 
-    test.arg1 should be("arg1")
+    test.arg1 should be(Some(10.seconds))
 
   }
 
   test("if arg is provided, ignore config file") {
 
-    val args = Array[String]("--arg1", "other")
+    val args = Array[String]("--arg1", "22.minutes")
 
     val test = new Test
     test.parse(args)
 
-    test.arg1 should be("other")
+    test.arg1 should be(Some(22.minutes))
 
   }
 
 
   test("support nested configs") {
 
-    val args = Array[String]("--arg1", "other")
+    val args = Array[String]("--arg1", "10 days")
 
     val test = new TestWithNested
     test.parse(args)
 
-    test.arg1 should be("other")
+    test.arg1 should be(Some(10.days))
     test.arg2 should not be (null)
-    test.arg2.arg3 should be("arg3")
+    test.arg2.arg3 should be(42.42)
 
   }
 
@@ -57,7 +58,7 @@ class ConfigTest extends FunSuite with ShouldMatchers {
 
     test.parse(Array[String]())
 
-    test.arg1 should be("alternate")
+    test.arg1 should be(Some(3 days))
 
   }
 
@@ -67,19 +68,19 @@ class ConfigTest extends FunSuite with ShouldMatchers {
 
     test.parse(Array[String]())
 
-    test.arg2.arg3 should be("default")
+    test.arg2.arg3 should be(10.5)
   }
 
   test("validations should be applied on the config value too") {
     val test = new Test {
       addValidation {
-        if (arg1.equals("arg1")) throw new IllegalArgumentException(s"test arg1 = '$arg1'")
+        if (arg1.equals(Some(10 seconds))) throw new IllegalArgumentException(s"test arg1 = $arg1")
       }
     }
     val ex = intercept[IllegalArgumentException] {
       test.parse(Array[String]())
     }
-    ex.getMessage should be("test arg1 = 'arg1'")
+    ex.getMessage should be("test arg1 = Some(10 seconds)")
   }
 
 }
@@ -87,17 +88,14 @@ class ConfigTest extends FunSuite with ShouldMatchers {
 class Test extends FieldArgs with ConfigArgs {
   override val configPrefix = "sumac.ext.test"
 
-  var arg1: String = _
+  var arg1: Option[Duration] = None
 }
 
 class Nested extends FieldArgs {
-  var arg3: String = "default"
+  var arg3: Double = 10.5
 }
 
 
-class TestWithNested extends FieldArgs with ConfigArgs {
-  override val configPrefix = "sumac.ext.test"
-
-  var arg1: String = _
+class TestWithNested extends Test {
   var arg2: Nested = new Nested
 }
