ded6ebf8d83b1f3d25e218c1a8e4e973dcdc9974
Imran Rashid <imran@quantifind.com>
2013-12-26 10:08:47 -0600

    bring up to date w/ master for 0.2 release, but still publish to our repo

--- project/Build.scala
+++ project/Build.scala
@@ -5,6 +5,7 @@ object SumacBuild extends Build {
   lazy val core = Project("core", file("core"), settings = coreSettings)
   lazy val ext = Project("ext", file("ext"), settings = extSettings) dependsOn(core)
 
+  val qf = "http://repo.quantifind.com/content/repositories/"
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     version := "0.2-SNAPSHOT",
     scalaVersion := "2.10.3",
@@ -13,59 +14,19 @@ object SumacBuild extends Build {
     unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
     retrieveManaged := true,
     transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
-    resolvers ++= Seq(
-      "sonatype-snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
-      "sonatype-releases"  at "http://oss.sonatype.org/content/repositories/releases",
-      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-      "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/"
-    ),
+    //publishTo <<= baseDirectory { base => Some(Resolver.file("Local", base / "target" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern))) },
+    publishTo <<= version {
+      (v: String) =>
+        Some("snapshots" at qf + "ext-snapshots")
+    },
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % "1.9.1" % "test"
     ),
-
-    // Publishing configuration
-
-    publishMavenStyle := true,
-
-    publishTo <<= version { (v: String) =>
-      val nexus = "https://oss.sonatype.org/"
-      if (v.trim.endsWith("SNAPSHOT"))
-        Some("snapshots" at nexus + "content/repositories/snapshots")
-      else
-        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-    },
-
-    publishArtifact in Test := false,
-
-    pomIncludeRepository := { x => false },
-
-    pomExtra := (
-      <url>https://github.com/quantifind/Sumac</url>
-      <licenses>
-        <license>
-          <name>Apache 2</name>
-          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-          <distribution>repo</distribution>
-          <comments>A business-friendly OSS license</comments>
-        </license>
-      </licenses>
-      <scm>
-        <url>git@github.com:quantifind/Sumac.git</url>
-        <connection>scm:git:git@github.com:quantifind/Sumac.git</connection>
-      </scm>
-      <developers>
-        <developer>
-          <id>imran</id>
-          <name>Imran Rashid</name>
-          <url>http://github.com/squito</url>
-        </developer>
-        <developer>
-          <id>ryan</id>
-          <name>Ryan LeCompte</name>
-          <url>http://github.com/ryanlecompte</url>
-        </developer>
-      </developers>)
+    resolvers ++= Seq(
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/"
     )
+  )
 
   val slf4jVersion = "1.6.1"
 
