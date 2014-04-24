bb0d39c0fb1ac1fdc5052ab1a9227e150ccc59a8
Pierre Andrews <pierre.andrews@gmail.com>
2014-04-24 14:36:04 -0300

    sbt-release with branching

--- project/Build.scala
+++ project/Build.scala
@@ -1,13 +1,24 @@
 import sbt._
-import Keys._
+import sbt.Keys._
+
+import sbtrelease.ReleaseStateTransformations._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.ReleasePlugin
+import sbtrelease.ReleaseStep
+import sbtrelease.Utilities._
+import sbtrelease.Vcs
+
+import annotation.tailrec
+
 
 object SumacBuild extends Build {
+  
   lazy val core = Project("core", file("core"), settings = coreSettings).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
   lazy val ext = Project("ext", file("ext"), settings = extSettings).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*) dependsOn(core)
   lazy val extZk = Project("ext-zk", file("ext-zk"), settings = extZkSettings).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*) dependsOn(core)
 
-  def sharedSettings = Defaults.defaultSettings ++ Seq(
-    version := "0.3.0-SNAPSHOT",
+  def sharedSettings = Defaults.defaultSettings ++ ReleasePlugin.releaseSettings ++ BranchRelease.branchSettings ++ Seq(
+    // version is managed by sbt-release in version.sbt
     scalaVersion := "2.11.0",
     organization := "com.quantifind",
     scalacOptions := Seq("-deprecation", "-unchecked", "-optimize"),
@@ -36,6 +47,21 @@ object SumacBuild extends Build {
     crossScalaVersions := Seq("2.10.3", "2.11.0"),
 
     // Publishing configuration
+
+    releaseProcess := Seq[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      runTest,
+      setReleaseVersion,
+      BranchRelease.makeBranch,
+      commitReleaseVersion,
+      publishArtifacts,
+      BranchRelease.pushBranch,
+      BranchRelease.moveToPreviousBranch,
+      setNextVersion,
+      commitNextVersion,
+      pushChanges
+    ),
 
     publishMavenStyle := true,
 
@@ -74,7 +100,12 @@ object SumacBuild extends Build {
           <id>ryan</id>
           <name>Ryan LeCompte</name>
           <url>http://github.com/ryanlecompte</url>
-        </developer>
+          </developer>
+        <developer>
+        <id>pierre</id>
+        <name>Pierre Andrews</name>
+        <url>http://github.com/Mortimerp9</url>
+          </developer>
       </developers>),
       javacOptions ++= Seq("-target", "1.6", "-source", "1.6")
     ) //++ ScoverageSbtPlugin.instrumentSettings ++ CoverallsPlugin.coverallsSettings // waiting for 2.11.0 release
@@ -103,4 +134,139 @@ object SumacBuild extends Build {
     )
   )
 
+}
+
+
+object BranchRelease {
+
+  import BranchReleaseKeys._
+
+  //> copy from sbtrelease some usefule private methods
+  private def vcs(st: State): Vcs = {
+    st.extract
+      .get(versionControlSystem)
+      .getOrElse(sys.error("Aborting release. Working directory is not a"+
+        "repository of a recognized VCS."))
+  }
+
+  private lazy val initialVcsChecks = { st: State =>
+    val status = (vcs(st).status !!).trim
+    if (status.nonEmpty) {
+      sys.error("Aborting release. Working directory is dirty.")
+    }
+    st
+  }
+
+  private lazy val checkUpstream = { st: State =>
+    if (!vcs(st).hasUpstream) {
+      sys.error("No tracking branch is set up. Either configure a remote tracking branch, or remove the pushChanges release part.")
+    }
+
+    st.log.info("Checking remote [%s] ..." format vcs(st).trackingRemote)
+    if (vcs(st).checkRemote(vcs(st).trackingRemote) ! st.log != 0) {
+      SimpleReader.readLine("Error while checking remote. Still continue (y/n)? [n] ") match {
+        case Yes() => // do nothing
+        case _ => sys.error("Aborting the release!")
+      }
+    }
+
+    if (vcs(st).isBehindRemote) {
+      SimpleReader.readLine("The upstream branch has unmerged commits. A subsequent push will fail! Continue (y/n)? [n] ") match {
+        case Yes() => // do nothing
+        case _ => sys.error("Merge the upstream commits and run `release` again.")
+      }
+    }
+    st
+  }
+  //<copy from sbtrelease
+
+  //////////////////////////////////////////////////////////////////////
+  // a release step that branches from the current branch
+  //////////////////////////////////////////////////////////////////////
+  lazy val makeBranch = ReleaseStep(makeBranchAction, initialVcsChecks)
+
+  private lazy val makeBranchAction = { st: State =>
+    val vc = vcs(st)
+
+    //store the current branch so we can come back here
+    val nst = st.put(previousBranchKey, vc.currentBranch)
+
+    //make sure that the new branch doesn't exist yet
+    @tailrec
+    def testBranch(branch: String): String = {
+      def localBranchExists = {
+       nst.log.info(s"checking if local $branch already exists")
+        (vc.cmd("branch") !!).linesIterator.exists {
+          _.endsWith(s" $branch")
+        }
+      }
+      def remoteBranchExists = {
+        nst.log.info(s"checking if remote $branch already exists")
+        (vc.cmd("ls-remote", "--heads") !!).linesIterator.exists {
+          _.endsWith(s"refs/heads/$branch")
+        }
+      }
+
+      //if the branch is already defined, get a new name
+      if(localBranchExists || remoteBranchExists) {
+        SimpleReader
+          .readLine(s"Branch [$branch] already exists, [a]bort or specify a new name: ") match {
+          case Some("" | "a" | "A") =>
+            sys.error(s"Branch [$branch] already exists. Aborting release!")
+          case Some(newBranch) =>
+            //test the entered name
+            testBranch(newBranch)
+          case None =>
+            sys.error(s"Branch [$branch] already exists. Aborting release!")
+        }
+      } else branch
+    }
+
+    //get the branch name from config
+    val (branchState, branch) = nst.extract.runTask(branchName, nst)
+    val branchToUse = testBranch(branch)
+    st.log.info("git branching sends its console output to standard error,"+
+      "which will cause the next few lines to be marked as [error].")
+    vc.cmd("checkout", "-b", branchToUse) !! branchState.log
+
+    //store the new branch to push it later
+    branchState.put(branchKey, branchToUse)
+  }
+
+  //////////////////////////////////////////////////////////////////////
+  //  push the release branch to origin TODO config the remote
+  //////////////////////////////////////////////////////////////////////
+  lazy val pushBranch = ReleaseStep(pushBranchAction, checkUpstream)
+  private lazy val pushBranchAction = { st: State =>
+    val vc = vcs(st)
+
+    val b = st.get(branchKey)
+      .getOrElse(sys.error("no branch set, you have to run this step after makeBranch"))
+    vc.cmd("push", "-u", "origin", b) !! st.log
+
+    st
+  }
+
+  lazy val moveToPreviousBranch: ReleaseStep = { st: State =>
+    val vc = vcs(st)
+
+    val ba = st.get(previousBranchKey)
+      .getOrElse(sys.error("no branch set, you have to run this step after makeBranch"))
+    vc.cmd("checkout", ba) !! st.log
+
+    st
+  }
+
+  lazy val branchSettings = Seq[Setting[_]](
+    branchName <<= (version in ThisBuild) map ( v => s"rel/$v" )
+  )
+
+}
+
+object BranchReleaseKeys {
+  //the name of the rel branch
+  lazy val branchName = TaskKey[String]("release-branch-name")
+  //used internaly to keep state
+  lazy val branchKey = AttributeKey[String]("release-branch")
+  lazy val previousBranchKey = AttributeKey[String]("current-branch")
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,3 +10,5 @@ addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
 //addSbtPlugin("org.scoverage" %% "sbt-coveralls" % "0.98.4")
 
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3")
+
